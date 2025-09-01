0e463877476876f57535613248cdc4c5b12f5242
Abdul-Mannan-123 <abdulmannan600306@gmail.com>
2025-09-01 23:33:13 +0500

    Task1.cpp

--- Lab1/Task1.cpp
+++ Lab1/Task1.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class BankAccount {
+    int balance;
+public:
+    BankAccount() { 
+		balance = 0;
+	}
+	
+    BankAccount(int b) { 
+		balance = b;
+	 }
+    BankAccount(const BankAccount& other) { 
+		balance = other.balance; 
+	}
+    void deduct(int amount) { 
+		balance -= amount;
+	}
+    int getBalance() { 
+		return balance; 
+	}
+};
+
 int main() {
-  cout << "HEllo World" << endl;
+    BankAccount account1;
+    cout << account1.getBalance() << endl;
+
+    BankAccount account2(1000);
+    cout << account2.getBalance() << endl;
+
+    BankAccount account3(account2);
+    account3.deduct(200);
+    cout << account3.getBalance() << endl;
+    cout << account2.getBalance() << endl;
+
+    return 0;
 }
