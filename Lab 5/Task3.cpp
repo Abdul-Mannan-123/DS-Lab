22a911ce6c79b4cfbb23d32b0a323029c948d566
Abdul-Mannan-123 <abdulmannan600306@gmail.com>
2025-09-22 01:48:10 +0500

    Create Task3.cpp

---
+++ Lab 5/Task3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumTail(int n, int total) {
+    if (n == 0) return total;
+    return sumTail(n - 1, total + n); 
+}
+
+int sumNonTail(int n) {
+    if (n == 0) return 0;
+    return n + sumNonTail(n - 1);
+}
+
+int main() {
+    int n = 10;
+    cout << "sumTail(10) = " << sumTail(n, 0) << '\n';
+    cout << "sumNonTail(10) = " << sumNonTail(n) << '\n';
+    return 0;
+}
